43145ced3ed6bcd3a5a2e7e5f4870d014c44daf5
Arun Gupta <arungup@arungup-mac.local>
2012-12-12 22:52:22 -0800

    Adding plugin.yml and adding a basic operation in listener

--- bukkitplugin1/src/main/java/org/sample/bukkitplugin1/MyPlayerListener.java
+++ bukkitplugin1/src/main/java/org/sample/bukkitplugin1/MyPlayerListener.java
@@ -1,7 +1,10 @@
 package org.sample.bukkitplugin1;
 
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.player.PlayerLoginEvent;
 
 /**
@@ -11,5 +14,15 @@ public class MyPlayerListener implements Listener {
     @EventHandler
     public void onPlayerLogin(PlayerLoginEvent event) {
         System.out.format("{0} logged in", (event.getPlayer().getPlayerListName()));
+    }
+    
+    @EventHandler
+    public void blockBreak(BlockBreakEvent event) {
+        Player player = event.getPlayer();
+        
+        if (!player.isOp()) {
+            event.setCancelled(true);
+            player.sendMessage(ChatColor.RED + "You are not allowed to break this block");
+        }
     }
 }
